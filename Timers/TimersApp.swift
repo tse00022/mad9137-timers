846c625a1b4aa09795575e29945230507073283c
Hiu Ming Tse <tse00022@algonquinlive.com>
2024-11-01 11:50:07 -0400

    project init

---
+++ Timers/TimersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimersApp.swift
+//  Timers
+//
+//  Created by EddieTse on 2024-11-01.
+//
+
+import SwiftUI
+
+@main
+struct TimersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
