626f3280d635ba3f638d8cdee393e3db29d48c82
Hiu Ming Tse <tse00022@algonquinlive.com>
2024-11-05 18:46:49 -0500

    TmerListView Update

--- Timers/Timer.swift
+++ Timers/Timer.swift
@@ -5,4 +5,16 @@
 //  Created by EddieTse on 2024-11-01.
 //
 
-import Foundation
+import SwiftUI
+
+struct Timer: Identifiable {
+    var id: UUID = UUID()
+    var duration: TimeInterval
+    var startTime: Date = Date()
+    var elapsed : TimeInterval {
+        Date().timeIntervalSince(startTime)
+    }
+    var isRunning: Bool {
+        elapsed < duration
+    }
+}
--- Timers/TimerListView.swift
+++ Timers/TimerListView.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct TimerListView: View {
+    @State var timers: [Timer] = [Timer(duration: 100)]
+    
+    
+    var body: some View{
+        NavigationStack{
+            List{
+                ForEach(timers){
+    
+                    timer in
+                    HStack{
+                        Text("\(timer.duration)")
+                        Button(action: {
+                        }){
+                            Text("Update")
+                        }
+                    }
+                    
+                }
+            }
+            .navigationTitle("Timers")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Add") {
+                        print("Add")
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+#Preview{
+    TimerListView()
+}
